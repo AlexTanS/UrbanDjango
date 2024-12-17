015034d808937c580f665b4424e7cd59a69c5749
test <60185787+AlexTanS@users.noreply.github.com>
2024-12-17 22:52:11 +0300

    module 18 5 create sign_up_by_django

--- task5/views.py
+++ task5/views.py
@@ -6,7 +6,29 @@ from .forms import UserRegister
 def sign_up_by_django(request: HttpRequest):
     users = ['alex', 'oleg', 'anna', 'tolik', 'john']
     info = {}
-    pass
+    if request.method == "POST":
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            # получаю данные
+            error = None
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            repeat_password = form.cleaned_data["repeat_password"]
+            age = form.cleaned_data["age"]
+            # обрабатываю данные
+            if password != repeat_password:
+                error = "Пароли не совпадают."
+            elif age < 18:
+                error = "Вы должны быть старше 18."
+            elif username in users:
+                error = "Пользователь уже существует"
+            else:
+                info["text"] = f"Приветствуем, {username}!"
+            info["error"] = error
+            info["form"] = form
+    else:
+        info["form"] = UserRegister()
+    return render(request=request, context=info, template_name="fifth_task/registration_page.html")
 
 
 def sign_up_by_html(request: HttpRequest):
